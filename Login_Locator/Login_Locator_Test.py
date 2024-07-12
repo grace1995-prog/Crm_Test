785100b3bddc7247a5b82b41ac0b5a295962c5d2
Mfonma uko <ukomfonmamonday@gmail.com>
2024-07-12 22:09:57 +0100

    test

---
+++ Login_Locator/Login_Locator_Test.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+class Login_Locator_Test:
+    ENTER_EMAIL_ADDRESS = (By.ID, "")
+    ENTER_PASSWORD = (By.ID, "")
+    REMEMBER_CHECK_BOX = (By.ID, "")
+    CLICK_SUBMIT_BUTTON = (By.ID, "")
---
+++ Login_Page/Login_Page_Test.py
@@ -0,0 +1,34 @@
+from selenium.webdriver.support.wait import WebDriverWait
+
+from selenium.webdriver.support import expected_conditions as EC
+
+from Login_Locator.Login_Locator_Test import Login_Locator_Test
+
+
+class Login_Page_Test:
+
+    def __init__(self, driver):
+        self.driver = driver
+
+    def login_url(self, url):
+        self.driver.get(url)
+
+    def email_address(self, email_address):
+        enter_email_address = WebDriverWait(self.driver, 20).until(
+            EC.presence_of_element_located(Login_Locator_Test.ENTER_EMAIL_ADDRESS))
+        enter_email_address.send_keys(email_address)
+
+    def enter_password(self, enter_password):
+        password_field = WebDriverWait(self.driver, 20).until(
+            EC.presence_of_element_located(Login_Locator_Test.ENTER_PASSWORD))
+        password_field.send_keys(enter_password)
+
+    def check_box(self, check_box):
+        checkbox_field = WebDriverWait(self.driver,10).until(
+            EC.presence_of_element_located(Login_Locator_Test.REMEMBER_CHECK_BOX))
+        checkbox_field.click()
+
+    def submit_button(self, submit_button):
+        button_field = WebDriverWait(self.driver, 20).until(
+            EC.presence_of_element_located(Login_Locator_Test.CLICK_SUBMIT_BUTTON))
+        button_field.click()
